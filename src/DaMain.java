1e9d6ac72a30fab7f011cb9af282c4be7ae59f64
JTBlanket <j.blankenship1717@gmail.com>
2017-05-15 12:51:53 -0600

    test

--- src/DaMain.java
+++ src/DaMain.java
@@ -3,9 +3,20 @@ public class DaMain {
 
 	public static void main(String[] args) {
 		System.out.println("Initial Build");
+
+		
+		System.out.println("Working of bug fix");
+		System.out.println("Bug Is FIXED!");
+		
+		
+		System.out.println("GoodLuck, trying new GUI ");
+		System.out.println("Whew it works");
+
+
 		System.out.println("GoodLuck, trying new GUI ");
 		System.out.println("Whew it works");
 		System.out.println("another GUI");
+
 	}
 
 }
